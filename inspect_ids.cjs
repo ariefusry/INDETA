b0dbe7172f5282040f4b4aef1ff19df184020f0b
Arief Muhammad Usry <ariefusry@student.telkomuniversity.ac.id>
2026-04-22 23:48:53 +0700

    Vercel Configuration

--- inspect_ids.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const txt = fs.readFileSync('temp_html.txt','utf8');
-const matches = [...txt.matchAll(/id=['"](.*?)['"]/g)];
-console.log(matches.map(m=>m[1]).join(', '));
--- split_admin.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('resources/views/admin/dashboard.blade.php', 'utf8');
-const scriptPart = c.substring(c.indexOf('<script '));
-const htmlPart = c.substring(0, c.indexOf('<script '));
-
-fs.writeFileSync('temp_html.txt', htmlPart);
-fs.writeFileSync('temp_script.txt', scriptPart);
